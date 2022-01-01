1b3162658c0a97bc8b6c1105dc8a55ef372334c2
Romel Campbell <RomelCampbell@gmail.com>
2022-01-01 05:47:51 -0500

    Struct example

--- udemy/Ethereum/contracts/MappingStructExample.sol
+++ udemy/Ethereum/contracts/MappingStructExample.sol
@@ -3,25 +3,45 @@
 pragma solidity ^0.8.1;
 
 contract MappingStructExample {
-	mapping(address => uint) public balanceReceived;
+
+	struct Payment {
+		uint amount;
+		uint timestamp;
+	}
+
+	struct Balance {
+		uint totalBalance;
+		uint numPayments;
+		mapping(uint => Payment) payments;
+	}
+
+	mapping(address => Balance) public balanceReceived;
 
 	function getBalance() public view returns (uint) {
 		return address(this).balance;
 	}
 
 	function sendMoney() public payable {
-		balanceReceived[msg.sender] += msg.value;
+		balanceReceived[msg.sender].totalBalance += msg.value;
+
+		Payment memory payment = Payment(msg.value, block.timestamp);
+
+		balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments] = payment;
+
+		balanceReceived[msg.sender].numPayments++;
+		//balanceReceived[msg.sender].totalBalance += payment.amount;
+
 	}
 
 	function withdrawMoney(address payable _to, uint _amount) public {
-		require(balanceReceived[msg.sender] >= _amount, "Not enough funds");
-		balanceReceived[msg.sender] -= _amount;
+		require(balanceReceived[msg.sender].totalBalance >= _amount, "Not enough funds");
+		balanceReceived[msg.sender].totalBalance -= _amount;
 		_to.transfer(_amount);
 	}
 
 	function withdrawAllMoney(address payable _to) public {
-		uint balanceToSend = balanceReceived[msg.sender];
-		balanceReceived[msg.sender] = 0;
+		uint balanceToSend = balanceReceived[msg.sender].totalBalance;
+		balanceReceived[msg.sender].totalBalance = 0;
 		_to.transfer(balanceToSend);
 	}
 }
