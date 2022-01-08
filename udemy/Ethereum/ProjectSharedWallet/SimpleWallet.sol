eae8ddb591c106bfcd8fed248d73006d4d70c757
Romel Campbell <RomelCampbell@gmail.com>
2022-01-08 05:54:01 -0500

    Adds event emmiters

--- udemy/Ethereum/ProjectSharedWallet/SimpleWallet.sol
+++ udemy/Ethereum/ProjectSharedWallet/SimpleWallet.sol
@@ -2,15 +2,55 @@
 
 pragma solidity ^0.8.1;
 
-import "./Owned.sol";
+//import "./Owned.sol";
 
-contract SimpleWallet is Owned {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-	function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+contract Allowance is Ownable {
+	event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount , uint _newAmount);
+
+	mapping(address => uint) public allowance;
+
+	function addAllowance(address _who, uint _amount) public onlyOwner{
+		emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+		allowance[_who] = _amount;
+	}
+
+
+    function isOwner() public view returns(bool) { 
+		return owner() == msg.sender;
+	}
+
+	function getBalance() public view returns (uint) {
+		return address(this).balance;
+	}
+
+	modifier ownerOrAllowed(uint _amount) {
+		require( isOwner() || allowance[msg.sender] >= _amount, "You ar not allowed");
+		_;
+	}
+
+	function reduceAllowance(address _who, uint _amount) internal {
+		emit AllowanceChanged(_who, msg.sender, allowance[_who],  allowance[_who] - _amount);
+		allowance[_who] -= _amount;
+	}
+}
+
+contract SimpleWallet is Allowance {
+
+	event MoneySent(address indexed _beneficiary, uint _amount);
+	event MoneyReceived(address indexed _from, uint _amount);
+
+	function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+		require(_amount <= address(this).balance, "There are not enough funds stored in the smart contract");
+		if (!isOwner()) {
+			reduceAllowance(msg.sender,_amount);
+		}
+		emit MoneySent(_to, _amount);
 		_to.transfer(_amount);
 	}
 
 	receive() external payable {
-
+		emit MoneyReceived(msg.sender, msg.value);
 	}
 }
