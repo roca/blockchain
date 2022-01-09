c441afb067e3be854091f06953fda9809e251e0c
Romel Campbell <RomelCampbell@gmail.com>
2022-01-09 05:23:00 -0500

    SimpleWallet done

--- udemy/Ethereum/ProjectSharedWallet/SimpleWallet.sol
+++ udemy/Ethereum/ProjectSharedWallet/SimpleWallet.sol
@@ -2,39 +2,7 @@
 
 pragma solidity ^0.8.1;
 
-//import "./Owned.sol";
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-
-contract Allowance is Ownable {
-	event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount , uint _newAmount);
-
-	mapping(address => uint) public allowance;
-
-	function addAllowance(address _who, uint _amount) public onlyOwner{
-		emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
-		allowance[_who] = _amount;
-	}
-
-
-    function isOwner() public view returns(bool) { 
-		return owner() == msg.sender;
-	}
-
-	function getBalance() public view returns (uint) {
-		return address(this).balance;
-	}
-
-	modifier ownerOrAllowed(uint _amount) {
-		require( isOwner() || allowance[msg.sender] >= _amount, "You ar not allowed");
-		_;
-	}
-
-	function reduceAllowance(address _who, uint _amount) internal {
-		emit AllowanceChanged(_who, msg.sender, allowance[_who],  allowance[_who] - _amount);
-		allowance[_who] -= _amount;
-	}
-}
+import "./Allowance.sol";
 
 contract SimpleWallet is Allowance {
 
@@ -50,6 +18,10 @@ contract SimpleWallet is Allowance {
 		_to.transfer(_amount);
 	}
 
+	function renounceOwnership() public virtual onlyOwner override {
+		revert("Can't renounce ownership here");
+	}
+	
 	receive() external payable {
 		emit MoneyReceived(msg.sender, msg.value);
 	}
