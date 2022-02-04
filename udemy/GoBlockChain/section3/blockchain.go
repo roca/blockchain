21ecde06115ecc38e49b21f8783c71937de9b382
Romel Campbell <RomelCampbell@gmail.com>
2022-02-04 10:41:18 -0500

    Init code

--- udemy/GoBlockChain/section3/blockchain.go
+++ udemy/GoBlockChain/section3/blockchain.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func (b Block) Print(){
+    fmt.Printf("Timestamp\t%d\n", b.timestamp)
+    fmt.Printf("Nonce\t\t%d\n", b.nonce)
+    fmt.Printf("PreviousHash\t%s\n", b.previousHash)
+    fmt.Printf("Transactions\t%s\n", b.transactions)
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
 
 func init() {
 	log.SetPrefix("Blockchain")
 }
 
 func main() {
-	log.Println("test")
-	fmt.Println("test")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
