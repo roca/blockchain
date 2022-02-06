19b76af7d81e271662e356ba7d52a8d0f04d9448
Romel Campbell <RomelCampbell@gmail.com>
2022-02-06 13:54:46 -0500

    ProofOfWork

--- udemy/GoBlockChain/section3/blockchain/blockchain.go
+++ udemy/GoBlockChain/section3/blockchain/blockchain.go
@@ -5,13 +5,15 @@ import (
 	"strings"
 )
 
+const MINING_DIFFICULTY = 3
+
 type Blockchain struct {
 	transactionPool []*Transaction
 	chain           []*Block
 }
 
 func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
-	b := NewBlock(nonce, previousHash,bc.transactionPool)
+	b := NewBlock(nonce, previousHash, bc.transactionPool)
 	bc.chain = append(bc.chain, b)
 	bc.transactionPool = []*Transaction{}
 	return b
@@ -20,6 +22,38 @@ func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 func (bc *Blockchain) AddTransaction(sender string, recipient string, value float32) {
 	t := NewTransaction(sender, recipient, value)
 	bc.transactionPool = append(bc.transactionPool, t)
+}
+
+func (bc *Blockchain) ValidProof(nonce int, previousHash [32]byte, transactions []*Transaction, difficulty int) bool {
+	zeros := strings.Repeat("0", difficulty)
+	guessBlock := Block{nonce: nonce, previousHash: previousHash, timestamp: 0, transactions: transactions}
+	guessHashStr := fmt.Sprintf("%x", guessBlock.Hash())
+	//fmt.Println(guessHashStr)
+	return guessHashStr[:difficulty] == zeros
+}
+
+func (bc *Blockchain) ProofOfWork() int {
+	transactions := bc.CopyTransactions()
+	previousHash := bc.LastBlock().Hash()
+	nonce := 0
+	for !bc.ValidProof(nonce, previousHash, transactions, MINING_DIFFICULTY) {
+		nonce += 1
+	}
+	return nonce
+}
+
+func (bc *Blockchain) CopyTransactions() []*Transaction {
+	transactions := make([]*Transaction, 0)
+	for _, t := range bc.transactionPool {
+		transactions = append(transactions,
+			NewTransaction(
+				t.senderBlockchainAddress,
+				t.recipientBlockchainAddress,
+				t.value,
+			),
+		)
+	}
+	return transactions
 }
 
 func (bc *Blockchain) Print() {
--- udemy/GoBlockChain/section3/main.go
+++ udemy/GoBlockChain/section3/main.go
@@ -17,13 +17,15 @@ func main() {
 	blockChain.AddTransaction("A", "B", 1.0)
 
 	previousHash := blockChain.LastBlock().Hash()
-	blockChain.CreateBlock(5, previousHash)	
+	nonce := blockChain.ProofOfWork()
+	blockChain.CreateBlock(nonce, previousHash)
 	blockChain.Print()
 
 	blockChain.AddTransaction("C", "D", 2.0)
 	blockChain.AddTransaction("X", "Y", 3.0)
 
 	previousHash = blockChain.LastBlock().Hash()
-	blockChain.CreateBlock(2,previousHash)
+	nonce = blockChain.ProofOfWork()
+	blockChain.CreateBlock(nonce, previousHash)
 	blockChain.Print()
 }
