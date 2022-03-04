3d160304eed6dc3e234c928b4d4672b31281f212
Romel Campbell <RomelCampbell@gmail.com>
2022-03-04 16:30:14 -0500

    Completed section 10

--- udemy/HyperLedgerFabric/HLF-Dev-Chaincode-V2.1-1.0/network/bin/_launch.sh
+++ udemy/HyperLedgerFabric/HLF-Dev-Chaincode-V2.1-1.0/network/bin/_launch.sh
@@ -1,4 +1,4 @@
 #PEER_MODE=net
-#Command=dev-init.sh -e 
-#Generated: Mon Feb 28 18:18:38 UTC 2022 
-docker-compose  -f ./compose/docker-compose.base.yaml      -f ./compose/docker-compose.explorer.yaml    up -d --remove-orphans
+#Command=dev-init.sh  
+#Generated: Fri Mar  4 21:02:41 UTC 2022 
+docker-compose  -f ./compose/docker-compose.base.yaml        up -d --remove-orphans
--- udemy/HyperLedgerFabric/HLF-Dev-Chaincode-V2.1-1.0/network/bin/cc.env.sh
+++ udemy/HyperLedgerFabric/HLF-Dev-Chaincode-V2.1-1.0/network/bin/cc.env.sh
@@ -1,22 +1,22 @@
-# Generated: Mon Feb 28 18:24:33 UTC 2022
+# Generated: Fri Mar  4 21:27:11 UTC 2022
 export CC_LANGUAGE=golang
-export CC_PATH=testing/cctest
-export CC_NAME=cctest
+export CC_PATH=token/v9
+export CC_NAME=token
 export CC_VERSION=1.0
 export CC_CHANNEL_ID=airlinechannel
-export CC_CONSTRUCTOR='{"Args":["init","a","100","b","300"]}'
-export CC_QUERY_ARGS='{"Args":["query","b"]}'
-export CC_INVOKE_ARGS='{"Args":["invoke","a","b","5"]}'
-export CORE_CHAINCODE_ID_NAME='cctest'
+export CC_CONSTRUCTOR='{"Args": ["init"]}'
+export CC_QUERY_ARGS='{"args":["get"]}'
+export CC_INVOKE_ARGS='{"args":["set", "UnProtectedToken","Budget"]}'
+export CORE_CHAINCODE_ID_NAME='token'
 export CORE_CHAINCODE_LOGGING_LEVEL=''
 export CORE_CHAINCODE_LOGGING_SHIM=''
 export CC_PRIVATE_DATA_JSON=''
 export CC_ENDORSEMENT_POLICY=""
-export CC2_SEQUENCE="1"
+export CC2_SEQUENCE="2"
 export CC2_INIT_REQUIRED="true"
 export CC2_PACKAGE_FOLDER="/home/vagrant/packages"
-export CC2_SIGNATURE_POLICY=""
+export CC2_SIGNATURE_POLICY="AND('BudgetMSP.member','AcmeMSP.member')"
 export CC2_CHANNEL_CONFIG_POLICY=""
-export CC2_ENDORSING_PEERS=""
+export CC2_ENDORSING_PEERS="both"
 export CC2_ENV_FOLDER="/vagrant/network/bin/env"
 export INTERNAL_DEV_VERSION="1"
