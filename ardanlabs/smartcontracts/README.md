0549b652aa631f95c0d72dcca8adc166d92cfeae
Romel Campbell <RomelCampbell@gmail.com>
2024-11-22 10:30:24 -0500

    Day 2 @0:17:00 in the video commit 15

--- ardanlabs/smartcontracts/README.md
+++ ardanlabs/smartcontracts/README.md
@@ -1,3 +1,3 @@
 # Course link [https://courses.ardanlabs.com/courses/take/smart-contracts-with-go/lessons/42928364-day-2-training-with-bill-kennedy-feb-14-2023](https://courses.ardanlabs.com/courses/take/smart-contracts-with-go/lessons/42928364-day-2-training-with-bill-kennedy-feb-14-2023)
 
-Day 2 @0:00:00 in the video
+Day 2 @0:17:00 in the video commit 15
---
+++ ardanlabs/smartcontracts/code/app/basic/contract/go/basic/basic_test.go
@@ -0,0 +1,105 @@
+package basic_test
+
+import (
+	"context"
+	"math/big"
+	"smartcontract/app/basic/contract/go/basic"
+	"testing"
+
+	"github.com/ardanlabs/ethereum"
+	"github.com/ardanlabs/ethereum/currency"
+)
+
+func TestBasic(t *testing.T) {
+	ctx := context.Background()
+
+	const numAccounts = 1
+	const autoCommit = true
+	var accountBalance = big.NewInt(100)
+
+	backend, err := ethereum.CreateSimulatedBackend(numAccounts, autoCommit, accountBalance)
+	if err != nil {
+		t.Fatalf("unable to create simulated backend: %s", err)
+	}
+	defer backend.Close()
+
+	clt, err := ethereum.NewClient(backend, backend.PrivateKeys[0])
+	if err != nil {
+		t.Fatalf("unable to create ethereum api: %s", err)
+	}
+
+	// =========================================================================
+
+	const gasLimit = 1600000
+	valueGwei := big.NewFloat(0.0)
+	gasPrice := currency.GWei2Wei(big.NewFloat(39.576))
+	tranOpts, err := clt.NewTransactOpts(ctx, gasLimit, gasPrice, valueGwei)
+	if err != nil {
+		t.Fatalf("unable to create transaction opts for deploy: %s", err)
+	}
+
+	contractID, tx, _, err := basic.DeployBasic(tranOpts, clt.Backend)
+	if err != nil {
+		t.Fatalf("unable to deploy basic: %s", err)
+	}
+
+	if _, err := clt.WaitMined(ctx, tx); err != nil {
+		t.Fatalf("waiting for deploy: %s", err)
+	}
+
+	testBasic, err := basic.NewBasic(contractID, clt.Backend)
+	if err != nil {
+		t.Fatalf("error creating basic: %s", err)
+	}
+
+	// =========================================================================
+
+	callOpts, err := clt.NewCallOpts(ctx)
+	if err != nil {
+		t.Fatalf("unable to create call opts: %s", err)
+	}
+
+	ver, err := testBasic.Version(callOpts)
+	if err != nil {
+		t.Fatalf("unable to get version: %s", err)
+	}
+
+	if ver != "1.1" {
+		t.Fatalf("should be able to get the correct version, got %s  exp %s", ver, "1.1")
+	}
+
+	// =========================================================================
+
+	tranOpts, err = clt.NewTransactOpts(ctx, gasLimit, gasPrice, valueGwei)
+	if err != nil {
+		t.Fatalf("unable to create transaction opts for setitem: %s", err)
+	}
+
+	key := "bill"
+	value := big.NewInt(1_000_000)
+
+	tx, err = testBasic.SetItem(tranOpts, key, value)
+	if err != nil {
+		t.Fatalf("should be able to set item: %s", err)
+	}
+
+	if _, err := clt.WaitMined(ctx, tx); err != nil {
+		t.Fatalf("waiting for setitem: %s", err)
+	}
+
+	// =========================================================================
+
+	callOpts, err = clt.NewCallOpts(ctx)
+	if err != nil {
+		t.Fatalf("unable to create call opts: %s", err)
+	}
+
+	item, err := testBasic.Items(callOpts, key)
+	if err != nil {
+		t.Fatalf("should be able to retrieve item: %s", err)
+	}
+
+	if item.Cmp(value) != 0 {
+		t.Fatalf("wrong value, got %s  exp %s", item, value)
+	}
+}
