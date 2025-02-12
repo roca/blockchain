18d274c5d9e471667736fe8402fb988b46a48b63
romel.campbell <romel.campbell@regeneron.com>
2025-02-12 14:21:11 -0500

    Proof of work func

--- udemy/GoBlockChain2/evochain/blockchain/blockchain.go
+++ udemy/GoBlockChain2/evochain/blockchain/blockchain.go
@@ -1,6 +1,10 @@
 package blockchain
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"evochain/constants"
+	"strings"
+)
 
 type Blockchain struct {
 	TransactionPool []*Transaction `json:"transaction_pool"`
@@ -27,4 +31,30 @@ func (bc *Blockchain) String() string {
 
 func (bc *Blockchain) AddTransaction(t *Transaction) {
 	bc.TransactionPool = append(bc.TransactionPool, t)
+}
+
+func (bc *Blockchain) ProofOfWork() *Block {
+	// calulate the PrevHash
+	prevHash := bc.Blocks[len(bc.Blocks)-1].Hash()
+	// start with a 0 nonce
+	nonce := 0
+	for {
+		// create a new block
+		guessBlock := NewBlock(prevHash, nonce)
+		// copy the transaction pool
+		for _, txn := range bc.TransactionPool {
+			newTxn := NewTransaction(txn.From, txn.To, txn.Value, txn.Data)
+			guessBlock.AddTransaction(newTxn)
+		}
+		// guess the Hash
+		guessHash := guessBlock.Hash()
+		desiredHash := strings.Repeat("0", constants.MINING_DIFFICULTY)
+		ourSolutionHash := guessHash[2 : 2+constants.MINING_DIFFICULTY]
+		// compare this hash with the mining difficulty
+		if ourSolutionHash == desiredHash {
+			return guessBlock
+		}
+		nonce++
+	}
+
 }
--- udemy/GoBlockChain2/evochain/constants/constants.go
+++ udemy/GoBlockChain2/evochain/constants/constants.go
@@ -5,4 +5,5 @@ const (
 	HEX_PREFIX      = "0x"
 	SUCCESS	 = "success"
 	FAILED	 = "failed"
+	MINING_DIFFICULTY = 4
 )
